6f8bdda2faf5a394754735bef6ecad0cbbe3aded
hideo54 <contact@hideo54.com>
2016-12-23 01:21:27 +0900

    Remove warning in announce view

--- main.js
+++ main.js
@@ -91,20 +91,7 @@ const proxy = http.createServer((req, res) => {
         agent: socket.$agent
     }, function onsrvRes(srvRes) {
         res.writeHead(srvRes.statusCode, srvRes.headers);
-        if ((reqUrl.hostname+reqUrl.path).indexOf(`${MAIN_HOST}/webview.php/announce/`) !== -1) {
-            let body = [];
-            srvRes.on('data', (chunk) => {
-                body.push(chunk);
-            }).on('end', () => {
-                body = Buffer.concat(body).toString();
-                let $ = cheerio.load(body);
-                $('body').prepend(`<h1 style="color: red;">You\'re using schfeslog!</h1>
-<p>Note that all of your actions are monitored.</p>`);
-                res.write($.html());
-            });
-        } else {
-            srvRes.pipe(res);
-        }
+        srvRes.pipe(res);
     });
     req.pipe(srvReq);
 
